015c434272c082be0ac055916441f83a5e497dfd
LittleDevil <119108710+LittleDevil013@users.noreply.github.com>
2023-05-08 09:13:30 +0200

    add playRound func

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choises = ['rock' , 'paper' , 'scissors'];
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     let randomValue = Math.floor(Math.random() * 3);
@@ -13,5 +15,28 @@ function getPlayerChoice() {
    return playerChoice;
 };
 
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+function playRound(playerSelection , computerSelection) {
+    let youWinText = "You Win! " +  playerSelection +  " beats " + computerSelection;
+    let youLoseText = "You Lose! " + computerSelection + " beats " + playerSelection;
+    let drawnText = "Drawn game!";
+
+    if ( computerSelection == 'rock' && playerSelection == 'paper' ||
+        computerSelection == 'paper' && playerSelection == 'scissors' || 
+        computerSelection == 'scissors' && playerSelection == 'rock')  {
+        return youWinText
+    }
+    else if (computerSelection == 'rock' && playerSelection == 'scissors' ||
+    computerSelection == 'paper' && playerSelection == 'rock' || 
+    computerSelection == 'scissors' && playerSelection == 'paper') {
+        return youLoseText
+    }
+    else if( computerSelection == playerSelection){
+        return drawnText
+
+    };
+
+};
+
+
+console.log(computerSelection);
+console.log(playRound(playerSelection , computerSelection))
