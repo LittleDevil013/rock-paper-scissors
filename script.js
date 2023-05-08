29aeb69acf8a02d81bff21d0e23c5db05c8eff8a
LittleDevil <119108710+LittleDevil013@users.noreply.github.com>
2023-05-08 10:38:25 +0200

    add game func

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const choises = ['rock' , 'paper' , 'scissors'];
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScoure = 0;
+
+
 
 function getComputerChoice() {
     let randomValue = Math.floor(Math.random() * 3);
@@ -15,28 +17,54 @@ function getPlayerChoice() {
    return playerChoice;
 };
 
-function playRound(playerSelection , computerSelection) {
+function playRound() {
+
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+
+
     let youWinText = "You Win! " +  playerSelection +  " beats " + computerSelection;
     let youLoseText = "You Lose! " + computerSelection + " beats " + playerSelection;
     let drawnText = "Drawn game!";
+    
 
     if ( computerSelection == 'rock' && playerSelection == 'paper' ||
         computerSelection == 'paper' && playerSelection == 'scissors' || 
         computerSelection == 'scissors' && playerSelection == 'rock')  {
+            playerScore++;
         return youWinText
     }
     else if (computerSelection == 'rock' && playerSelection == 'scissors' ||
     computerSelection == 'paper' && playerSelection == 'rock' || 
     computerSelection == 'scissors' && playerSelection == 'paper') {
+        computerScoure++;
         return youLoseText
     }
-    else if( computerSelection == playerSelection){
+    else if( computerSelection == playerSelection) {
+        playerScore++;
+        computerScoure++;
         return drawnText
 
     };
 
 };
 
+function game() {
 
-console.log(computerSelection);
-console.log(playRound(playerSelection , computerSelection))
+    for (i = 1; i <= 5; i++){
+        console.log(playRound());
+    };
+
+};
+
+game();
+
+if (playerScore > computerScoure) {
+    console.log("You win the Game!!!")
+}
+else if(computerScoure > playerScore){
+    console.log("You lost the Game!!!")
+};
+
+
+
